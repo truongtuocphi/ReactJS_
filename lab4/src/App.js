ae92b33e634c5646295a4c5559ec2368fedcb62e
unknown <truongphi754@gmail.com>
2023-07-22 18:55:15 +0700

    new Update

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -1,13 +1,37 @@
 // import logo from './logo.svg';
 import './App.css';
-import Search from './Component/Search'
+import Search from './Component/Search';
+import React, { Component } from 'react';
+import  axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
+export default class App extends Component {
+  state = { image: [] }
+
+  onSearchSubmit = async (term) => {
+    const reponse = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+        'Client-ID 92cEjPgQH9ucxVAqH8o2rCus0w8sgAtsqsswDT3xyYU',
+      },
+    });
+    this.setState({ image: reponse.data.results });
+    console.log(reponse.data.results);
+  }
+
+  render() {
+    return (
+      <div>
+        <Search onSubmit={this.onSearchSubmit} />
+        {/* Found: { this.state.image.length } images */}
+        <div className='container px-20 grid mx-auto gap-2 grid-cols-4'>
+          { this.state.image.map((items, index) => (
+            <div key={index} className=''>
+              <img className='rounded-lg h-full'  src={items.urls.regular} alt="" />
+            </div>
+          )) }
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
--- lab4/src/Component/Search.js
+++ lab4/src/Component/Search.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 export default class Search extends Component {
     state = { term: '' };
 
-    API_kEY = 'rcydsMMCtThSv5Q14vsTwOArhBzeMl58OjcFYSr-BUU'; 
-
     onFromSubmit = (event) => {
         event.preventDefault();
-        // console.log(this.state.term);
-        try {
-            // const reponse = await axios.get(this.Ap)
-        }catch {
-
-        }
+        console.log(this.state.term);
+        this.props.onSubmit(this.state.term);
     }
 
     render() {
         return (
-            <div className='w-full flex justify-center pt-2'>
-                <form onSubmit={this.onFromSubmit}>
-                    <label>Image Search</label>
+            <div className='w-full flex justify-center pt-3 mb-5'>
+                <form onSubmit={this.onFromSubmit} className='w-3/4 h-10 relative'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="#5F5F5F" className="w-5 h-5 absolute left-3 top-3 translate-x-[-50] translate-y-[-50]">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    </svg>
                     <input
                         type="text"
+                        placeholder='Search'
                         // value={this.state.term}
                         onChange={e => this.setState({ term: e.target.value })}
-                        className='border-2'
+                        className='border-2 container h-full rounded-full outline-0 px-10 py-5 text-lg bg-slate-100'
                     />
                 </form>
             </div>
