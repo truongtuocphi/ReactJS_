5987fec37cc82cebbeecdd31b1bd2ff9f0bbc627
unknown <truongphi754@gmail.com>
2023-07-23 20:46:31 +0700

    update layout lab4

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -24,10 +24,10 @@ export default class App extends Component {
       <div>
         <Search onSubmit={this.onSearchSubmit} />
         {/* Found: { this.state.image.length } images */}
-        <div className='container px-20 grid mx-auto gap-2 grid-cols-4'>
+        <div className='container px-[7%] mx-auto columns-3'>
           { this.state.image.map((items, index) => (
-            <div key={index} className=''>
-              <img className='rounded-lg h-full'  src={items.urls.regular} alt="" />
+            <div key={index} className='flex mb-4'>
+              <img className='rounded-lg'  src={items.urls.regular} alt="" />
             </div>
           )) }
         </div>
--- lab4/src/Component/Search.js
+++ lab4/src/Component/Search.js
@@ -13,7 +13,7 @@ export default class Search extends Component {
     render() {
         return (
             <div className='w-full flex justify-center pt-3 mb-5'>
-                <form onSubmit={this.onFromSubmit} className='w-3/4 h-10 relative'>
+                <form onSubmit={this.onFromSubmit} className='w-[85%] h-10 relative'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="#5F5F5F" className="w-5 h-5 absolute left-3 top-3 translate-x-[-50] translate-y-[-50]">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
