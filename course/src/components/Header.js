334139f71b81ca65628c344cc544c682e5bdcec2
unknown <truongphi754@gmail.com>
2023-08-02 09:47:02 +0700

    Header

---
+++ course/src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import logo from '../logo.png'
+
+export default function Header() {
+  return (
+    <header className='w-full bg-white px-10 py-3 grid grid-cols-4 content-center'>
+        <div className='col-span-1 flex items-center gap-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:cursor-pointer">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+            </svg>
+            <img src={ logo } alt={ logo } />
+        </div>
+        <div className='col-span-2 w-full flex bg-gray-100 px-5 py-2 rounded-full'>
+            <input className='bg-gray-100 border-none outline-none w-full text-lg' placeholder='Tìm kiếm khóa học, bài học...' />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+        </div>
+        <div className='flex justify-end gap-4 items-center'>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                </svg>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+            </div>
+        </div>
+    </header>
+  )
+}
---
+++ course/src/components/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Heroicons } from 'heroicons';
+
+export default function Menu({ icon, text }) {
+  return (
+    <div>
+      <i className={Heroicons.academicCap}></i>
+    </div>
+  )
+}
